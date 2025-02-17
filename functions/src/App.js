2424b66444cee8bd2c839a739c119d9711efccd8
James <james@steanemurphy.com>
2025-02-17 20:20:35 +1100

    functions: body component finished, media breakpoints, styling

--- functions/src/App.js
+++ functions/src/App.js
@@ -25,6 +25,40 @@ const carData = [
   },
 ];
 
+const languages = ["English", "Spanish", "French", "German", "Chinese", "Japanese", "Korean", "Russian", "Portuguese", "Arabic", 
+  "Hindi", "Italian", "Dutch", "Turkish", "Swedish"];
+
+const driverKnowledge = ["restaurants", "museums", "parks", "nightlife", "historical sites", "shopping", "public transport", "local events", 
+  "best coffee spots", "hidden gems"];
+
+const funFacts = [
+  "I am a certified grandmaster in Chess!",
+  "I once drove across the entire country in a week!",
+  "I can solve a Rubik's Cube in under a minute!",
+  "I know five different languages!",
+  "I used to be a professional race car driver!",
+  "I have a collection of over 100 vintage license plates!",
+  "I once gave a ride to a famous celebrity!",
+  "I can play the guitar and sing at the same time!",
+  "I love discovering the best hidden food spots in the city!",
+  "I have completed a marathon in under four hours!"
+];
+
+const driverReasons = [
+  "I wanted a flexible work schedule.",
+  "I enjoy meeting new people.",
+  "It’s a great way to earn extra income.",
+  "I love driving and exploring new places.",
+  "It allows me to work when I want.",
+  "I appreciate the independence it offers.",
+  "I wanted to make use of my car.",
+  "It fits well with my other commitments.",
+  "I enjoy the freedom of being my own boss.",
+  "It’s a fun and dynamic job."
+];
+
+  
+
 //LOGO COMPONENTS
 const Logo = () => {
 
@@ -80,7 +114,91 @@ const DriverStats = () => {
   );
 };
 
-const ProfileHeader = () => {
+const ProfileHeader = ({ driver }) => {
+
+  return (
+    <div className="headerContainer">
+      <ProfileImage image={driver.picture.large} />
+      <DriverName name={driver.name} />
+      <DriverStats />
+    </div>
+  );
+};
+
+//BODY COMPONENTS
+const DriverDetails = ({ driver }) => {
+  const language = languages[Math.floor(Math.random() * languages.length)];
+  const knowledge = driverKnowledge[Math.floor(Math.random() * driverKnowledge.length)];
+  const funFact = funFacts[Math.floor(Math.random() * funFacts.length)];
+  const reason = driverReasons[Math.floor(Math.random() * driverReasons.length)];
+
+  return (
+    <div className="detailsContainer">
+      <div>
+        <img src="./images/web.png" alt="icon"/>
+        <span>Speaks</span>&nbsp;<span style={{color: "black"}}><b>{language}</b></span>
+      </div>
+      <div>
+        <img src="./images/location.png" alt="icon"/>      
+        <span>From</span>&nbsp;<span style={{color: "black"}}><b>{driver.location.city}, {driver.location.country}</b></span>    
+      </div>
+      <div>
+        <img src="./images/question.png" alt="icon"/>       
+        <span>Ask about</span>&nbsp;<span style={{color: "black"}}><b>{knowledge}</b></span> 
+      </div>           
+      <hr></hr>
+      <div className="double">
+        <div>
+          <span style={{color: "black", fontSize: "1.1em"}}>Fun Fact</span>
+        </div>
+        <div>
+          <img src="./images/message.png" alt="icon"/>
+          <span>{funFact}</span>
+        </div>
+      </div>    
+      <hr></hr>
+      <div className="double">
+        <div>
+          <span style={{color: "black", fontSize: "1.1em"}}>Why Uber?</span>
+        </div>
+        <div className="quote">
+          <img src="./images/quote.jpg" alt="icon"/>  
+          <span>{reason}</span>
+        </div>    
+      </div>
+      <hr></hr>
+    </div>
+  );
+};
+
+const SocialLinks = () => {
+
+  return (
+    <div className="socialContainer">
+      <div className="socialImageContainer">
+        <img src="./images/facebook.png" alt="Facebook Link"/>
+      </div>
+      <div className="socialImageContainer">
+        <img src="./images/instagram.png" alt="Instagram Link"/>
+      </div>
+      <div className="socialImageContainer">
+        <img src="./images/twitter.png" alt="X Link"/>
+      </div>
+    </div>
+  );
+};
+
+const ProfileBody = ({ driver }) => {
+
+  return (
+    <div className="bodyContainer">
+      <DriverDetails driver={driver} />
+      <SocialLinks />
+    </div>
+  );
+};
+
+function App() {
   const [driver, setDriver] = useState(null);
 
   //fetch random user from free api
@@ -99,67 +217,10 @@ const ProfileHeader = () => {
   }
 
   return (
-    <div className="headerContainer">
-      <ProfileImage image={driver.picture.large} />
-      <DriverName name={driver.name} />
-      <DriverStats />
-    </div>
-  );
-};
-
-//BODY COMPONENTS
-const DriverDetails = () => {
-
-  return (
-    <div className="detailsContainer">
-      <div>
-        <img src="./images/web.png" alt="icon"/>
-        <p>Speaks English and German</p>
-      </div>
-      <div>
-        <img src="./images/web.png" alt="icon"/>
-        <p>From Vancouver, Canada</p>
-      </div>
-      <div>
-        <img src="./images/web.png" alt="icon"/>
-        <p>Ask about restraunts and musuems</p>
-      </div>
-      <hr></hr>
-      <div>
-        <img src="./images/web.png" alt="icon"/>
-        <p>Fun Fact</p>
-        <p>I am a certified grandmaster at Chess!</p>
-      </div>
-      <hr></hr>
-    </div>
-  );
-};
-
-const SocialLinks = () => {
-
-  return (
-    <div className="socialContainer">
-      
-    </div>
-  );
-};
-
-const ProfileBody = () => {
-
-  return (
-    <div className="bodyContainer">
-      <DriverDetails />
-      <SocialLinks />
-    </div>
-  );
-};
-
-function App() {
-  return (
     <div className="mainContainer">
       <Logo />
-      <ProfileHeader />
-      <ProfileBody />
+      <ProfileHeader driver={driver} />
+      <ProfileBody driver={driver} />
     </div>
   );
 }
