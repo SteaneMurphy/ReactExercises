d56dfb17fa4ef989003e77ee32aaa8f570037716
James <james@steanemurphy.com>
2025-02-17 21:23:33 +1100

    state: invite button functionality

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = ({ invites }) => {
 
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>Invite people to Dribble</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <span>{invites} invites available</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+  );
+};
+
+const ProfileCard = () => {
+  
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+const InviteEmail = () => {
+  
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+const Footer = ({ setInvites, invites }) => {
+  const handleInvite = () => {
+    if (invites > 0) {
+      setInvites(invites => invites - 1);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleInvite}>INVITE</button>
+    </div>
+  );
+};
+
+function App() {
+  const [invites, setInvites] = useState(5);
+
+  return (
+    <div className="mainContainer">
+      <Header invites={invites} />
+      <Footer setInvites={setInvites} invites={invites} />
+    </div>
   )
 }
 
