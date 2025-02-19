b6b248bf7ddf1ccc374c40b8dfc58284e9055b3b
James <james@steanemurphy.com>
2025-02-19 17:15:59 +1100

    weatherAPI: grabs data from weather service and displays to screen

--- weatherAPI/js/scripts.js
+++ weatherAPI/js/scripts.js
@@ -1,2 +1,10 @@
-
 var URL = "http://api.openweathermap.org/data/2.5/weather?q=Sydney&units=metric&APPID=0467bbad2230162c7dc089ff287cdc7f";
+
+function readAPIData(data){
+    document.getElementById("weather").innerHTML += 
+    `Current Temperature in ${data.name} is ${data.main.temp} degrees`;
+};
+
+fetch(URL)
+.then(response => response.json())
+.then(data => readAPIData(data));
